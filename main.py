ca5c7060e9ec7bb0a6029f3224756f56cf2da880
Lymmak <mlymar-ki22@stud.sfu-kras.ru>
2024-01-22 15:18:57 +0700

    2. Добавляем функцию вычислений

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 def calculate_semiperimeter(a, b, c):
     return (a + b + c) / 2
 
-
+def calculate_area(a, b, c):
+    s = calculate_semiperimeter(a, b, c)
+    area = (s * (s - a) * (s - b) * (s - c)) ** 0.5
+    return area
 
 if __name__ == '__main__':
     pass
